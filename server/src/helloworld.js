e245f7dc98e41ff0a8c1f95ae6d966525d98640b
Kevin Silva <kevinsilva93@hotmail.com>
2016-12-01 16:25:53 -0500

    helloworld.js util.js and image added

---
+++ server/src/helloworld.js
@@ -0,0 +1,6 @@
+console.log("Hello World!");
+
+var util = require("./util");
+var reverse = util.reverseString;
+
+console.log(reverse("Hello World!"));
---
+++ server/src/util.js
@@ -0,0 +1,11 @@
+function reverseString (str) {
+  var reversed = "";
+
+  for (var i=1; i<=str.length; i++) {
+    reversed += str[str.length - i];
+  }
+
+  return reversed;
+}
+
+module.exports.reverseString = reverseString;
