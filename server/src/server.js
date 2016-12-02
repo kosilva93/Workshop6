8033b943d26055033fc67f64debb5bfa28d3414d
Kevin Silva <kevinsilva93@hotmail.com>
2016-12-01 23:43:15 -0500

    validation

--- server/src/server.js
+++ server/src/server.js
@@ -3,13 +3,15 @@ var app = express();
 
 var database = require('./database');
 var readDocument = database.readDocument;
+var addDocument = database.addDocument;
+var writeDocument = database.readDocument;
+
+var StatusUpdateSchema = require('./schemas/statusupdate.json');
+var validate = require('express-jsonschema').validate;
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.text());
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+app.use(bodyParser.json());
 
 app.use(express.static('../client/build'));
 
@@ -44,7 +46,7 @@ app.get('/user/:userid/feed', function(req, res) {
 // URL parameters are stored in req.params
 var userid = req.params.userid;
 // Send response.
-//res.send(getFeedData(userid));
+res.send(getFeedData(userid));
 });
 
 function getUserIdFromToken(authorizationLine) {
@@ -74,9 +76,64 @@ app.get('/user/:userid/feed', function(req, res) {
   var useridNumber = parseInt(userid, 10);
   if (fromUser === useridNumber) {
 
-    //res.send(getFeedData(userid));
+    res.send(getFeedData(userid));
   } else {
 
     res.status(401).end();
   }
+});
+
+function postStatusUpdate(user, location, contents) {
+  var time = new Date().getTime();
+
+  var newStatusUpdate = {
+    "likeCounter": [],
+    "type": "statusUpdate",
+    "contents": {
+      "author": user,
+      "postDate": time,
+      "location": location,
+      "contents": contents,
+      "likeCounter": []
+    },
+    "comments": []
+  };
+
+  newStatusUpdate = addDocument('feedItems', newStatusUpdate);
+
+  var userData = readDocument('users', user);
+  var feedData = readDocument('feeds', userData.feed);
+  feedData.contents.unshift(newStatusUpdate._id);
+
+  writeDocument('feeds', feedData);
+
+  return newStatusUpdate;
+}
+
+app.post('/feeditem', validate({ body: StatusUpdateSchema }), function(req, res) {
+  var body = req.body;
+  var fromUser = getUserIdFromToken(req.get('Authorization'));
+
+  if (fromUser === body.userId) {
+    var newUpdate = postStatusUpdate(body.userId, body.location,
+      body.contents);
+      res.status(201);
+      res.set('Location', '/feeditem/' + newUpdate._id);
+      res.send(newUpdate);
+    } else {
+
+      res.status(401).end();
+    }
+});
+
+app.use(function(err, req, res, next) {
+  if (err.name === 'JsonSchemaValidation') {
+    res.status(400).end();
+  } else {
+    next(err);
+  }
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
 });
