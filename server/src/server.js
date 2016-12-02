c6269ddaaf1c73782f7bb0be0549131e8e3ffc40
Kevin Silva <kevinsilva93@hotmail.com>
2016-12-01 20:27:54 -0500

    static

--- server/src/server.js
+++ server/src/server.js
@@ -8,19 +8,8 @@ var bodyParser = require('body-parser');
 
 app.use(bodyParser.text());
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
 });
 
-app.listen(5000, function () {
-  console.log('Example app listening on port 5000!');
-});
-
-app.post('/reverse', function (req, res) {
-  if(typeof(req.body) === 'string') {
-    var reversed = reverseString(req.body);
-    res.send(reversed);
-  } else {
-    res.status(400).end();
-  }
-});
+app.use(express.static('../client/build'));
